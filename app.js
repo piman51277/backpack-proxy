8af074110e23479f6036ec3751ec9320403d23c5
piman <aidenleepi@gmail.com>
2020-10-04 21:38:31 -0400

    replace global rate limit with queue system

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const app = express();
-const SKU = require('tf2-sku');
 const axios = require('axios');
 
-const unixTime = () => { return Math.round((new Date()).getTime() / 1000); }
-
-let lastRequest = 0;
+const queue = []
 
 //Checks to see if config vars exist;
 if (!process.env.BPAPIKEY) throw ('Missing Backpack API Key.')
 if (!process.env.APIKEY) throw ('Missing API Key.')
 
 app.get('/forward', (req, res) => {
-    //global rate limit
-    if (unixTime() <= lastRequest) {
-        res.status(429).send(JSON.stringify({ 'error': 'rate exceeded' }))
-    }
 
     //Checks to see if the API key is valid.
     if (req.query.apikey != process.env.APIKEY) {
         res.status(403).send(JSON.stringify({ 'error': 'invalid api key' }))
     }
 
+    queue.push(()=>{
     //sets API key in the request.
     let cdata ={
         ...JSON.parse(req.query.params),
         ...{'key':process.env.BPAPIKEY}
 }
-    
-    
-    console.log(cdata);
-    //sets lastRequest
-    lastRequest = unixTime();
     //sends request
     axios.get('https://backpack.tf/api/classifieds/search/v1', {
         'params': cdata
@@ -41,6 +30,14 @@ app.get('/forward', (req, res) => {
         console.log(`ERROR ${err}`)
         res.status(400).send(JSON.stringify({ 'error': err.message }))
     })
+    })
 })
 
 app.listen(process.env.PORT || 8000, () => {})
+
+setInterval(()=>{
+    if(queue[0] != undefined){
+        queue[0]();
+        queue.shift();
+    }
+},1010)
